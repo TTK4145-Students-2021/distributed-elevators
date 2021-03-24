f96d8849db11b22532b90c459d0de6b92a56dc4f
Martin Heir <mbheir@gmail.com>
2021-03-24 16:13:04 +0100

    added some controller logic, not tested

--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-	"./hardware_io"
 	"fmt"
 	"sync"
+
+	"./controller_fsm"
+	"./hardware_io"
 )
 
-const num_floors = 4
-const 
-
 func main() {
+	const N_FLOORS = 4
 
 	var wg sync.WaitGroup
 	wg.Add(1)
 
-	fmt.Printf("Starting Elevator")
-	go hardware_io.StartElevatorHardware(num_floors)
+	fmt.Println("### Starting Elevator ###")
+	go hardware_io.StartElevatorHardware(N_FLOORS)
+	go controller_fsm.StartElevatorController()
+	// go elevio.PollButtons()
 
 	wg.Wait()
 }
