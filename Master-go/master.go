50b97045ac98bade956c8f2383a7e7f24b1620a3
eskilbo <eskilbo@stud.ntnu.no>
2021-03-24 14:13:38 +0100

    Update master.go

--- Master-go/master.go
+++ Master-go/master.go
@@ -3,10 +3,23 @@ package master
 import (
   "Network-go"
   "ftm"
-  ""
 )
 
 /*
 Master-modulen får inn ordre og elevator states.
 Regner ut ny fordeling av ordre og sender denne ut.
 */
+
+for{
+  select{
+    /*
+    hvis master:
+    du får en updated state og updated order inn på channel
+    når du får state/order, regner du ut hva ny ordreliste skal være og sender ut til alle og lagrer ny state og order
+    */
+  }
+}
+
+func calculate_distribution(states, orders) {
+
+}
