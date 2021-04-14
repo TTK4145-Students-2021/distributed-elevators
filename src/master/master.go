33b9d7810c8154db9e0f1d9584980aa7feceb387
Martin Heir <mbheir@gmail.com>
2021-04-14 15:47:58 +0200

    changes in master.go

--- src/master/master.go
+++ src/master/master.go
@@ -1,8 +1,7 @@
-package master
+package main
 
 import (
-  "Network-go"
-  "ftm"
+	"os/exec"
 )
 
 /*
@@ -10,16 +9,20 @@ Master-modulen får inn ordre og elevator states.
 Regner ut ny fordeling av ordre og sender denne ut.
 */
 
-for{
-  select{
-    /*
-    hvis master:
-    du får en updated state og updated order inn på channel
-    når du får state/order, regner du ut hva ny ordreliste skal være og sender ut til alle og lagrer ny state og order
-    */
-  }
+func runMaster() {
+	for {
+		select {
+		/*
+		   hvis master:
+		   du får en updated state og updated order inn på channel
+		   når du får state/order, regner du ut hva ny ordreliste skal være og sender ut til alle og lagrer ny state og order
+		*/
+		}
+	}
 }
 
-func calculate_distribution(states, orders) {
+func calculate_distribution() {
+
+	cmd := exec.Command("../", "serv.dev")
 
 }
