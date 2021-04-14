db3b3e28fa8a8aee8b3b2d8484c195c774b8cb4d
Martin Heir <mbheir@gmail.com>
2021-04-14 15:04:52 +0200

    added types and hall_req_assigner

---
+++ src/types/types.go
@@ -0,0 +1,37 @@
+
+/* Variables */
+const N_FLOORS = 4
+const N_BUTTONS = 3
+
+/* Types */
+type Behavior int
+
+const (
+	BH_Idle Behavior = iota
+	BH_Moving
+	BH_DoorOpen
+)
+
+type Dir int
+
+const (
+	DIR_Up = iota
+	DIR_Down
+)
+
+type ButtonType int
+
+const (
+	BT_HallUp   ButtonType = 0
+	BT_HallDown            = 1
+	BT_Cab                 = 2
+)
+
+type Elevator struct {
+	Behavior  Behavior
+	Direction Dir
+	Floor     int
+	Availeble bool
+	Orders    [N_FLOORS][N_BUTTONS]bool
+	Lights    [N_FLOORS][N_BUTTONS]bool
+}
