f9b7c5b9d41209b252925ccce3dab30f75d66c49
Håvard <hava.brenne@gmail.com>
2021-04-14 17:47:30 +0100

    Added channel send based on channel string adress

--- src/Network/network/messaging/Server.go
+++ src/Network/network/messaging/Server.go
@@ -3,31 +3,34 @@ package jsonpipe
 import (
 	"bufio"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
 	"net"
+	"reflect"
 )
+
 type TestMSG struct {
-	number int
+	number  int
 	message string
 }
 type RXChannels struct {
 	/*StateCh chan //elev.state
 	OrderUpdateCH chan //OrderUpdateCH
 	AllOrdersCH chan //orders */
-	TestCH chan TestMSG
+	TestCh1 chan TestMSG
+	TestCh2 chan TestMSG
 }
 type Server struct {
-	rxChannels 	RXChannels
-	Reader         *bufio.Reader
-	Encoder        *json.Encoder
+	rxChannels RXChannels
+	Reader     *bufio.Reader
+	Encoder    *json.Encoder
 }
 
 type Request struct {
-	ModuleAdress string                 `json:"mAdd"`
-	RequestId    string                 `json:"reqId"`
-	Data         map[string]interface{} `json:"data"`
+	ChannelAdress string `json:"mAdd"`
+	RequestId     string `json:"reqId"`
+	//Data          map[string]interface{} `json:"data"`
+	Data []byte `json:"data"`
 }
 
 type Message struct {
@@ -35,9 +38,9 @@ type Message struct {
 	Data       []byte
 }
 
-func NewServer() *Server {
+func NewServer(rxChs RXChannels) *Server {
 	server := Server{
-		ActionRegistry: make(map[string]Action),
+		rxChannels: rxChs,
 	}
 	return &server
 }
@@ -108,15 +111,34 @@ func (server Server) HandleMessage(msg Message) {
 	if err := json.Unmarshal(msg.Data, &request); err != nil {
 		log.Println("Error decoding JSON:" + err.Error())
 	}
+	w := reflect.ValueOf(server.rxChannels)
 
-	switch request.ModuleAdress{
-		case "master"
-			masterCh<-request.Data
-		case "orders"
-			ordersCh<-request.Data
-		case "peer"
-			peerCh<-request.Data
+	for i := 0; i < w.NumField(); i++ {
+		ch := w.Field(i).Interface()
+		//for _, ch := range server.rxChannels {
+
+		T := reflect.TypeOf(ch).Elem()
+		typeName := T.String()
+		if request.ChannelAdress == typeName {
+			//if strings.HasPrefix(string(request.Data)+"{", typeName) {
+			v := reflect.New(T)
+			json.Unmarshal(request.Data, v.Interface())
+
+			reflect.Select([]reflect.SelectCase{{
+				Dir:  reflect.SelectSend,
+				Chan: reflect.ValueOf(ch),
+				Send: reflect.Indirect(v),
+			}})
+		}
 	}
+	/*switch request.ModuleAdress{
+		case "TestCh1"
+			orderUpdate OrderUpdate{}
+			err := mapstructure.Decode(input, &restaurant)
+			server.rxChannels.TestCh1<-request.Data
+		case "TestCh2"
+			server.rxChannels.TestCh2<-request.Data
+	}*/
 
 	/*bytes, err := json.Marshal(response)
 	if err != nil {
