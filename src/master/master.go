134a01baf778733eddd5a00633f1bb947e28b190
Martin Heir <mbheir@gmail.com>
2021-04-16 18:29:42 +0200

    minor changes

--- src/master/master.go
+++ src/master/master.go
@@ -1,9 +1,7 @@
-package main
+package master
 
 import (
 	"encoding/json"
-	"fmt"
-	"strings"
 
 	// "io/ioutil"
 	"os/exec"
@@ -11,7 +9,7 @@ import (
 	// "reflect"
 	// "reflect"
 	. "../types"
-	"github.com/davecgh/go-spew/spew"
+	// "github.com/davecgh/go-spew/spew"
 )
 
 /*Types*/
@@ -27,42 +25,19 @@ type SingleElevator struct {
 	CabRequests [N_FLOORS]bool `json:"cabRequests"`
 }
 
-type AssignReqInput struct {
-	HallRequests [N_FLOORS][N_BUTTONS - 1]bool `json:"hallRequests"`
+type CombinedElevators struct {
+	GlobalOrders [N_FLOORS][N_BUTTONS - 1]bool `json:"hallRequests"`
 	States       map[string]SingleElevator     `json:"states"`
 }
-type OrderMatrix [N_FLOORS][N_BUTTONS]bool
-
-type GlobalOrderMap map[string]OrderMatrix
 
 func main() {
 	/* 	channels */
 
 	/* 	variables */
-
-	/* 	map["id"] SingleElevator
-	   	global_orders [N_FLOOR][2] `json:"hallRequests"`
-
-	*/
-	global_orders := [N_FLOORS][N_BUTTONS - 1]bool{}
-	e_states := make(map[string]SingleElevator)
-
-	spew.Dump("spew\n")
-	fmt.Println("fmt")
-	elev := SingleElevator{"moving", 2, "up", [N_FLOORS]bool{false, false, true, true}}
-
-	// spew.Dump(elev)
-	e_states["heis1"] = elev
-
-	// spew.Dump(e_states)
-
-	global_orders[1][0] = true
-	global_orders[3][1] = true
-
-	input := AssignReqInput{global_orders, e_states}
-	global_order_map := calculateDistribution(input.Json())
-
-	fmt.Println(global_order_map)
+	// combined := CombinedElevators{
+	// 	GlobalOrders: [N_FLOORS][N_BUTTONS - 1]bool{},
+	// 	States:       make(map[string]SingleElevator),
+	// }
 
 	for {
 		select {
@@ -99,8 +74,8 @@ func main() {
 	}
 }
 
-func (in AssignReqInput) Json() string {
-	json_byte, _ := json.Marshal(&in)
+func (c CombinedElevators) Json() string {
+	json_byte, _ := json.Marshal(&c)
 	return string(json_byte)
 }
 
@@ -120,27 +95,4 @@ func check(e error) {
 	if e != nil {
 		panic(e)
 	}
-}
-
-func (mat OrderMatrix) String() string {
-	var s []string
-	for i := range mat {
-		s = append(s, fmt.Sprintf("f%d: ", i+1))
-		for _, n := range mat[i] {
-			b2i := map[bool]int8{false: 0, true: 1}
-			s = append(s, fmt.Sprintf("%d   ", b2i[n]))
-		}
-		s = append(s, "\n")
-	}
-	return strings.Join(s, "")
-}
-
-func (m GlobalOrderMap) String() string {
-	var s []string
-	for name, mat := range m {
-		// fmt.Printf("teller")
-		s = append(s, fmt.Sprintln(name+":"))
-		s = append(s, fmt.Sprint(mat))
-	}
-	return strings.Join(s, "")
 }
--- src/types/types.go
+++ src/types/types.go
@@ -1,10 +1,15 @@
 package types
 
+import (
+	"fmt"
+	"strings"
+)
+
 /* Variables */
 const N_FLOORS = 4
 const N_BUTTONS = 3
 
-/* Types */
+/* #### Types #### */
 type Behavior int
 
 const (
@@ -28,7 +33,11 @@ const (
 	BT_Cab                 = 2
 )
 
-/* Structs */
+type OrderMatrix [N_FLOORS][N_BUTTONS]bool
+
+type GlobalOrderMap map[string]OrderMatrix
+
+/* #### Structs ####*/
 
 type State struct {
 	Behavior  Behavior `json:"behavior"`
@@ -37,8 +46,31 @@ type State struct {
 	Availeble bool
 }
 
-/* Basic member functions */
+/* #### Basic member functions #### */
 func (d Dir) String() string {
 	a := []string{"DIR_Up", "DIR_Down"}
 	return a[int(d)]
+}
+
+func (mat OrderMatrix) String() string {
+	var s []string
+	for i := range mat {
+		s = append(s, fmt.Sprintf("f%d: ", i+1))
+		for _, n := range mat[i] {
+			b2i := map[bool]int8{false: 0, true: 1}
+			s = append(s, fmt.Sprintf("%d   ", b2i[n]))
+		}
+		s = append(s, "\n")
+	}
+	return strings.Join(s, "")
+}
+
+func (m GlobalOrderMap) String() string {
+	var s []string
+	for name, mat := range m {
+		// fmt.Printf("teller")
+		s = append(s, fmt.Sprintln(name+":"))
+		s = append(s, fmt.Sprint(mat))
+	}
+	return strings.Join(s, "")
 }
