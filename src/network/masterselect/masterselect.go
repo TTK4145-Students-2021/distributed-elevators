1a5e99017e0861e1dc2dc11a54f7b610e277d06f
Håvard <hava.brenne@gmail.com>
2021-04-19 15:51:04 +0200

    Removed some print statements

--- src/network/masterselect/masterselect.go
+++ src/network/masterselect/masterselect.go
@@ -10,7 +10,7 @@ import (
 
 func DetermineMaster(id string, currentMasterId string, connectedPeers []peers.Peer, isMaster chan<- bool) string {
 	//Sort all peers, signal if we are lowest id
-	fmt.Println("peers detrmining", connectedPeers)
+	//fmt.Println("peers detrmining", connectedPeers)
 	var peers []int
 	currentlyMaster := id == currentMasterId
 	idInt, err := strconv.Atoi(id)
@@ -18,20 +18,17 @@ func DetermineMaster(id string, currentMasterId string, connectedPeers []peers.P
 		fmt.Println("Error: This elevator id is not a int, reboot with proper integer id")
 	}
 	//peers := make([]string, len(connectedPeers))
-	fmt.Println("Created peers: ", peers)
 	for _, p := range connectedPeers {
-		fmt.Println("Added peer ", p.Id)
 		pInt, _ := strconv.Atoi(p.Id)
 		peers = append(peers, pInt)
 	}
 	sort.Ints(peers)
-	fmt.Println("Sorted peers id: ", peers)
 	fmt.Printf("Elevator %s: Master is elevator %d\n", id, peers[0])
 
 	if peers[0] == idInt && !currentlyMaster {
 		isMaster <- true
 	} else if peers[0] != idInt && currentlyMaster {
-		fmt.Println("Removed as master")
+		//fmt.Println("Removed as master")
 		isMaster <- false
 	}
 	currentMasterId = strconv.Itoa(peers[0])
